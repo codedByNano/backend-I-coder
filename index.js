b51fd8e0c98ea8116903c7e0a5b794bf50cd2498
gNano <martinianogancedo@hotmail.com>
2024-06-27 16:34:03 -0300

    actividades 1 y 2

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import http from "node:http";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(201).json({
+    mensaje: "Todo ok",
+  });
+});
+
 app.listen(8080, () => {
   console.log("Servidor listo, escuchando en puerto 8080");
 });
